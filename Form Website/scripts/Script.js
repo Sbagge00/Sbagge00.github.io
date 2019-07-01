2a41bddf46ce99c76241ca36eacbff9ee841cea0
Stephen <31675411+Sbagge00@users.noreply.github.com>
2019-07-01 10:07:35 -0500

    Add files via upload

---
+++ Form Website/scripts/Script.js
@@ -0,0 +1,9 @@
+/* placeholder for button function */
+function myFunction() {
+    var txt;
+    var person = alert("Invalid username or password.");
+}
+function emailPopup() {
+    var txt;
+    var message = alert("If there is an account linked with that email, we will send you a link to reset your password. Please allow 1-5 minutes of processing time. Check your spam folder.");
+}
